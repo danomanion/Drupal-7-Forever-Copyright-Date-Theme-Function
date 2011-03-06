26ef7df2bb6903b3ee4f5790a720ffba1344ce50
Dano Manion <danomanion@gmail.com>
2011-03-05 17:35:58 -0700

    added the code

--- page.tpl.php
+++ page.tpl.php
@@ -1,1 +1,6 @@
 <!-- Add this to your Drupal page.tpl.php Template --> 
+<!-- Usage, the first varaible is for Anytext wanted. -->
+<!-- The second variable is for any seperator wanted. -->
+<!-- The third is for your INITIAL copyright date, so 1998 or something -->
+
+<?php  print yourthemenamehere_copyrightdate('Copyright ',' &mdash; ','2010');  ?>    
--- template.tpl.php
+++ template.tpl.php
@@ -1,2 +1,22 @@
 <?php /* Add this to your Drupal Theme Template */ ?>
- 
+
+<?php
+
+//Function to add Auto-Updating Copyright Date
+function yourthemenamehere_copyrightdate($copyrightText, $copyrightSeperator, $copyrightDate) {
+	//Set Variables
+	$output = '';
+	$copyrightCurrentDate = Date('Y');
+  		
+  	$output = $copyrightText . $copyrightDate;
+  	
+  	//Has the website outlasted a year? TEST IT!	
+  	if ($copyrightDate != $copyrightCurrentDate) {
+ 		$output .= $copyrightSeperator . $copyrightCurrentDate;
+  		};
+
+	return $output;
+}
+
+
+?> 
